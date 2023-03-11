bbd15f8e23a91ccaeca941590f94ad0d0a2ff366
Phil Wang <lucidrains@gmail.com>
2023-03-10 18:51:44 -0800

    fix l2 attention

--- gigagan_pytorch/gigagan_pytorch.py
+++ gigagan_pytorch/gigagan_pytorch.py
@@ -131,7 +131,7 @@ class Attention(nn.Module):
         q, k, v = self.to_qkv(fmap).chunk(3, dim = 1)
         q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> (b h) (x y) d', h = self.heads), (q, k, v))
 
-        sim = torch.cdist(q, k, p = 2) # l2 distance
+        sim = -torch.cdist(q, k, p = 2) * self.scale # l2 distance
 
         attn = sim.softmax(dim = -1)
 
