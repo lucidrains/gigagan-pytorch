3fbdc33cdaa28016a81a1e4b5e7b3afc9ad8ebee
Phil Wang <lucidrains@gmail.com>
2023-04-04 13:07:22 -0700

    assert

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'gigagan-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.8',
+  version = '0.0.9',
   license='MIT',
   description = 'GigaGAN - Pytorch',
   author = 'Phil Wang',
