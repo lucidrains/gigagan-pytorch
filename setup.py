8d46eb383376526e1fdd16da8d4d4656f56ab385
Phil Wang <lucidrains@gmail.com>
2023-04-04 13:04:02 -0700

    0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'gigagan-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'GigaGAN - Pytorch',
   author = 'Phil Wang',
